629cd72ca2580e91a4bdc05ccf977048cdc6265c
dorian <dimitri200169@gmail.com>
2025-11-19 20:32:16 +0100

    added minimum length as a argument

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-ml", "--max-length", help="length of the sound file")
+    parser.add_argument("--max-length", help="maximum length of the sound file")
+    parser.add_argument("--max-length", help="minimum length of the sound file")
     parser.add_argument("-t", "--tags", nargs="*", help="the audio files will have either one of those tags in the defined list")
     parser.add_argument("-f", "--formats", nargs="*", help="the audio files will have either one of those audio format defined in the list")
 
