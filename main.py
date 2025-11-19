abe45adcf1733348acd5be1fbfcc9597ca273740
dorian <dimitri200169@gmail.com>
2025-11-19 20:26:14 +0100

    argparse

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from client import Client
-
-
-
-
-
-
-
-
-
-
-
+import argparse
 
 
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ml", "--max-length", help="length of the sound file")
+    parser.add_argument("-t", "--tags", nargs="*", help="the audio files will have either one of those tags in the defined list")
+    parser.add_argument("-f", "--formats", nargs="*", help="the audio files will have either one of those audio format defined in the list")
 
 
+    args = parser.parser_args()
